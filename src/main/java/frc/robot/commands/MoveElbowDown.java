bf6346797af2879ee741b620a9d4c5ae71a54cc4
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2019-01-23 17:15:39 -0800

    Added Move Arm Commands

---
+++ src/main/java/frc/robot/commands/MoveElbowDown.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class MoveElbowDown extends Command {
+  private final double ELBOW_SPEED = 0.6;
+  public MoveElbowDown() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.m_elbowJoint.moveJointMotor(ELBOW_SPEED);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.m_elbowJoint.moveJointMotor(0.0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
--- src/main/java/frc/robot/subsystems/ARMJoint.java
+++ src/main/java/frc/robot/subsystems/ARMJoint.java
@@ -70,4 +70,8 @@ public class ARMJoint extends Subsystem {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
+
+  public void moveJointMotor(double speed) {
+   m_jointMotorGroup.set(speed); 
+  }
 }
